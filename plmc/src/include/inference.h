cdabe25c6bede6633046470600d11be90cb7fe1a
Thomas Hopf <thomas.hopf@gmail.com>
2017-01-14 22:23:28 -0500

    Removed plmc subtree

--- plmc/src/include/inference.h
+++
@@ -1,7 +0,0 @@
-#ifndef INFERENCE_H
-#define INFERENCE_H
-
-/* Estimates parameters of maximum entropy model */
-lbfgsfloatval_t *InferPairModel(alignment_t *ali, options_t *options);
-
-#endif /* INFERENCE_H */
